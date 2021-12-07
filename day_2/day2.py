1a66053b2acd7647579daf315be540cda8b99025
Ryan Cheevers-Brown <ryan@cheevtech.com>
2021-12-06 23:23:16 -0500

    part 1 completed

--- day_2/day2.py
+++ day_2/day2.py
@@ -1,25 +1,19 @@
 import os, sys, time, math
 
 class position:
-    def __init__(self):
-        self.x_pos = 0
-        self.y_pos = 0
+    def __init__(self, x_pos=0, y_pos=0):
+        self.x_pos = x_pos
+        self.y_pos = y_pos
     
-    @property
     def change_x_pos(self, amount):
-        self.x_pos += amount
-        return self.x_pos
+        self.x_pos = self.x_pos + amount
 
-    @property
     def change_y_pos(self, amount):
         self.y_pos += amount
-        return self.y_pos
 
-    @property
     def get_x(self):
         return self.x_pos
 
-    @property
     def get_y(self):
         return self.y_pos
 
@@ -31,17 +25,19 @@ def main():
     with open("input.txt", "r") as f:
         for line in f:
             commands.append(line.strip())
-    pos = position()
+    #print(commands[0])
+    posit = position()
     for command in commands:
         cmd = command.split(" ")
+        #print(f"{int(cmd[1])}")
         if cmd[0] == "forward":
-            pos.change_x_pos(int(cmd[1]))
+            posit.change_x_pos(int(cmd[1]))
         elif cmd[0] == "up":
-            pos.change_y_pos(-1*int(cmd[1]))
+            posit.change_y_pos(-1*int(cmd[1]))
         elif cmd[0] == "down":
-            pos.change_y_pos(int(cmd[1]))
+            posit.change_y_pos(int(cmd[1]))
 
-    print(f"{pos.get_x()*pos.get_y()}")
+    print(f"{posit.get_x()*posit.get_y()}")
 
 
 if __name__ == "__main__":
