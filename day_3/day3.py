8b13cff107b49367979284a35c5981b4c3f26a65
Ryan Cheevers-Brown <ryan@cheevtech.com>
2021-12-08 13:13:22 -0500

    Day 3 completed

--- day_3/day3.py
+++ day_3/day3.py
@@ -1,7 +1,9 @@
 import os, sys, time, math, copy
 
+dpr = True
+
 def dprint(arg):
-    dpr = False
+    global dpr
     if dpr == True:
         print(arg)
 
@@ -24,73 +26,105 @@ def binaryToDecimal(binary):
 
 
 def main():
-    matrix = list()
+    global dpr
+    matrix, gamma, epsilon, o2, co2 = list(), list(), list(), list(), list()
     with open("input.txt", "r") as f:
         for line in f:
             matrix.append([char for char in line.strip()])
 
-    out = []
-    gamma = []
-    epsilon = []
-
     for i in range(len(matrix[0])):
-        out.append(0)
-
-
-    for i in range(len(matrix[0])):
-        ones = 0
-        zeros = 0
+        ones, zeros = 0, 0
         for j in range(len(matrix)):
-            if matrix[j][i] == "0":
+            if str(matrix[j][i]) == "0":
                 zeros += 1
             else:
                 ones += 1
         dprint(f"{ones}, {zeros}")
         if ones > zeros:
-            out[i] = 1
-        else:
-            out[i] = 0
-            
-
-    for item in out:
-        if item == 0:
-            gamma.append(0)
-            epsilon.append(1)
-        else:
             gamma.append(1)
             epsilon.append(0)
+        elif ones == zeros:
+            gamma.append(1)
+            epsilon.append(0)
+        else:
+            gamma.append(0)
+            epsilon.append(1)
     
     print(f"\ngamma = {gamma}, epsilon = {epsilon}")
     print(f"dec(gamma) = {binaryToDecimal(gamma)}, dec(epsilon) = {binaryToDecimal(epsilon)}")
     print(f"part1 answer={binaryToDecimal(gamma)*binaryToDecimal(epsilon)}\n")
 
-    o2 = list()
-    co2 = list()
-
     orig = copy.deepcopy(matrix)
 
-    dprint(f"filtering matrix by gamma:   {gamma}")
-    o2 = filter_matrix(gamma, matrix)
-    dprint(matrix)
-    dprint(f"filtering matrix by epsilon: {epsilon}")
-    co2 = filter_matrix(epsilon, orig)
-    dprint(orig)
+    o2_filter, co2_filter = list(), list()
+    co2_flag, o2_flag = False, False
+    for i in range(len(matrix[0])):
+        val = 0
+        if len(matrix) == 1:
+            o2_filter = matrix[0]
+            o2_flag = True
+        if len(matrix) == 1:
+            o2 = matrix[0]
+            break
+        
+        dpr = False
+        for j in range(len(matrix)):
+            val += int(matrix[j][i])
+        if 2*val > len(matrix):
+            o2_filter.append(1)
+            dprint("Case 1")
+        elif 2*val == len(matrix):
+            o2_filter.append(1)
+            dprint("Case 2")
+        else: #val < 500
+            dprint("Case 3")
+            o2_filter.append(0)
+        dpr = True
+        dprint(f"val={val}")
+        matrix = filter_matrix(o2_filter, matrix, i)
+        
+
+    for i in range(len(orig[0])):
+        val = 0
+        if len(orig) == 1:
+            co2 = orig[0]
+            break
+
+        dpr = False
+        for item in orig:
+            val += int(item[i])
+        if 2*val > len(orig):
+            co2_filter.append(0)
+            dprint("Case 1")
+        elif 2*val == len(orig):
+            co2_filter.append(0)
+            dprint("Case 2")
+        else: #val < 500
+            co2_filter.append(1)
+            dprint("Case 3")
+        dpr = True
+        dprint(f"val={val}")
+        orig = filter_matrix(co2_filter, orig, i)
+
+
+    o2 = matrix[0]
+    co2 = orig[0]
 
     for i in range(len(o2)):
         o2[i] = int(o2[i])
         co2[i] = int(co2[i])
 
-    print(f"\ngamma = {gamma}, epsilon = {epsilon}")
-    print(f"o2 =    {o2}, co2 =     {co2}")
+    print(f"\no2_filter = {o2_filter}, co2_filter = {co2_filter}")
+    print(f"o2 =        {o2}, co2 =        {co2}")
     print(f"dec(o2) = {binaryToDecimal(o2)}, dec(co2) = {binaryToDecimal(co2)}")
     print(f"part2 answer = {binaryToDecimal(o2)*binaryToDecimal(co2)}\n")
 
 
-def filter_matrix(target, matrix):
-    for i in range(len(matrix[0])):
-        dprint(len(matrix))
+def filter_matrix(target, matrix, i):
+    try:
+        #dprint(len(matrix))
         if len(matrix) == 1:
-            dprint("\n")
+            #dprint("\n")
             return matrix[0]
         value = target[i]
         removed = 0
@@ -100,7 +134,11 @@ def filter_matrix(target, matrix):
             else:
                 matrix.remove(matrix[j-removed])
                 removed += 1
-    raise ValueError("There is an error in your code")
+        return matrix
+    except Exception as e:
+        print(f"target={target}, matrix, i={i}")
+        raise(e)
+    #raise ValueError("There is an error in your code")
     
             
 
