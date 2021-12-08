cf5904836f502d95643ffb797ef2ef4cd4a2dd55
Ryan Cheevers-Brown <ryan@cheevtech.com>
2021-12-08 12:15:11 -0500

    debugging second part...unsure why it's not working.

--- day_3/day3.py
+++ day_3/day3.py
@@ -1,17 +1,27 @@
-import os, sys, time, math
+import os, sys, time, math, copy
 
-def decimalToBinary(n):
-    return bin(n).replace("0b","")
+def dprint(arg):
+    dpr = False
+    if dpr == True:
+        print(arg)
 
 def binaryToDecimal(binary):
-    binary1 = binary
-    decimal, i, n = 0, 0, 0
-    while(binary != 0):
-        dec = binary % 10
-        decimal = decimal + dec * pow(2, i)
-        binary = binary//10
-        i += 1
-    return decimal   
+    bin=list()
+    if type(binary) == str:
+        bin.append(int(char) for char in binary)
+    elif type(binary) == list:
+        bin = copy.deepcopy(binary)
+        for i in range(len(bin)):
+            bin[i] = int(bin[i])
+    bin.reverse()
+    iter = 1
+    dec = 0
+    for num in bin:
+        if num == 1:
+            dec += iter
+        iter *= 2
+    return dec
+
 
 def main():
     matrix = list()
@@ -25,60 +35,74 @@ def main():
 
     for i in range(len(matrix[0])):
         out.append(0)
-    
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            #print(j)
-            if matrix[i][j] == "1":
-                out[j] += 1
+
+
+    for i in range(len(matrix[0])):
+        ones = 0
+        zeros = 0
+        for j in range(len(matrix)):
+            if matrix[j][i] == "0":
+                zeros += 1
             else:
-                out[j] -= 1
-    #print(out)
+                ones += 1
+        dprint(f"{ones}, {zeros}")
+        if ones > zeros:
+            out[i] = 1
+        else:
+            out[i] = 0
+            
 
     for item in out:
-        if item > 0:
-            gamma.append(1)
-            epsilon.append(0)
-        else:
+        if item == 0:
             gamma.append(0)
             epsilon.append(1)
-
-    
-    gam_val = ""
-    eps_val = ""
-
-    gamma.reverse()
-    epsilon.reverse()
-
-    iter = False
-    print(f"{gamma}, {epsilon}")
-
-    for item in gamma:
-        print(item)
-        if item == 0 and iter == False:
-            pass
         else:
-            iter = True
-            gam_val += str(item)
-            print(gam_val)
-    #print()
-    iter = False
-    for item in epsilon:
-        #print(item)
-        if item == 0 and iter == False:
-            pass
-        else:
-            iter = True
-            eps_val += str(item)
+            gamma.append(1)
+            epsilon.append(0)
     
-    print(f"{gam_val}, {eps_val}")
+    print(f"\ngamma = {gamma}, epsilon = {epsilon}")
+    print(f"dec(gamma) = {binaryToDecimal(gamma)}, dec(epsilon) = {binaryToDecimal(epsilon)}")
+    print(f"part1 answer={binaryToDecimal(gamma)*binaryToDecimal(epsilon)}\n")
 
-    #gam_val = gam_val[::-1]
-    #eps_val = eps_val[::-1]
+    o2 = list()
+    co2 = list()
 
-    print(f"{gam_val}, {eps_val}")
-    #print(f"{binaryToDecimal(gam_val)}*{binaryToDecimal(eps_val)}")
-    #print(binaryToDecimal(gam_val))
+    orig = copy.deepcopy(matrix)
+
+    dprint(f"filtering matrix by gamma:   {gamma}")
+    o2 = filter_matrix(gamma, matrix)
+    dprint(matrix)
+    dprint(f"filtering matrix by epsilon: {epsilon}")
+    co2 = filter_matrix(epsilon, orig)
+    dprint(orig)
+
+    for i in range(len(o2)):
+        o2[i] = int(o2[i])
+        co2[i] = int(co2[i])
+
+    print(f"\ngamma = {gamma}, epsilon = {epsilon}")
+    print(f"o2 =    {o2}, co2 =     {co2}")
+    print(f"dec(o2) = {binaryToDecimal(o2)}, dec(co2) = {binaryToDecimal(co2)}")
+    print(f"part2 answer = {binaryToDecimal(o2)*binaryToDecimal(co2)}\n")
+
+
+def filter_matrix(target, matrix):
+    for i in range(len(matrix[0])):
+        dprint(len(matrix))
+        if len(matrix) == 1:
+            dprint("\n")
+            return matrix[0]
+        value = target[i]
+        removed = 0
+        for j in range(len(matrix)):
+            if str(value) == str(matrix[j-removed][i]):
+                pass
+            else:
+                matrix.remove(matrix[j-removed])
+                removed += 1
+    raise ValueError("There is an error in your code")
+    
+            
 
 if __name__ == "__main__":
     main()
